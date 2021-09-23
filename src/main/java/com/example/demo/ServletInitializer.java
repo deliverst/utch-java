09cbe04d044845d6507d0265d159b7c8d225d31e
Javier Valles <deliverst@icloud.com>
2021-09-23 14:22:30 -0600

    add config connection database

--- src/main/java/com/example/demo/ServletInitializer.java
+++ src/main/java/com/example/demo/ServletInitializer.java
@@ -21,6 +21,9 @@ public class ServletInitializer extends SpringBootServletInitializer {
 //mvn spring-boot:run
 // crear carpeta app dentro de /Users/deliverst/gitProyects/javaDemo/src/main/java/com/example/demo/"app"
 
+
+//controlador | es una capa de nuestra aplicacion que se va a encargar de recibir las peticionees del cliente de un web browser, que utulizando un metodo post,get, put, delet el controlador canaliza a las diferencetes funciones
+
 /*
 java
 jdk
@@ -32,4 +35,31 @@ maven
 insomnia
 postman
 sptringboot
+
+
+
+
+es una especificacion de java en como se debe de manejar la base de datos desde java <artifactId>spring-boot-starter-data-jpa</artifactId> jpa: Java Persistence API, nos ayuda a manera la base de datos desde codigo y nos sirve de intermediario
+
+
+➜  javaDemo git:(dev) ✗ psql postgres
+psql (13.4)
+Type "help" for help.
+
+
+➜  javaDemo git:(dev) ✗ brew services start postgresql
+postgres=# create database tid71d;
+CREATE DATABASE
+postgres=#
+
+
+
+
+
+
+
+
+
 */
+
+
--- src/main/java/com/example/demo/app/users/restcontroller/UserRestController.java
+++ src/main/java/com/example/demo/app/users/restcontroller/UserRestController.java
@@ -1,4 +1,28 @@
 package com.example.demo.app.users.restcontroller;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
 public class UserRestController {
+
+    @GetMapping("/{id}")
+    public Integer findById(@PathVariable("id")Integer id){
+        return id;
+    }
+
+
+    @PostMapping
+    public String create(@RequestBody String usuario){
+        return usuario;
+    }
 }
+
+
+/* http://localhost:8080/users/
+
+{
+    "usuario":"hola"
+}
+
+ */
