ad095a250bcadb215d5e0f025ee0cd3d23660965
Javier Valles <deliverst@icloud.com>
2021-10-11 18:44:54 -0600

    generate code for upload files, but upload in / & without extension

--- src/main/java/com/example/demo/app/documents/DocumentsRestController.java
+++ src/main/java/com/example/demo/app/documents/DocumentsRestController.java
@@ -1,5 +1,6 @@
 package com.example.demo.app.documents;
 
+import com.example.demo.components.FileManagment;
 import com.example.demo.utils.Router;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
+import java.io.IOException;
 
 @RestController
 @RequestMapping(Router.DOCUMENTS)
@@ -16,12 +17,25 @@ public class DocumentsRestController {
 
     @Value("${bucket}")
     private String bucket;
+    private final FileManagment fileManagment;
 
-    @PostMapping("/upload")
-    public void upload(@RequestParam("file")MultipartFile multipartFile,
-                        @RequestParam("name") String name) {
-
-        //Nombre del Archivo
-        System.out.println(bucket + File.separator +multipartFile.getOriginalFilename());
+    public DocumentsRestController(FileManagment fileManagment) {
+            this.fileManagment = fileManagment;
         }
+
+        @PostMapping("/upload")
+        public void upload(@RequestParam("file")MultipartFile multipartFile,
+                            @RequestParam("name") String name) {
+            try{
+                fileManagment.upload(multipartFile,name);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
     }
+
+
+//Nombre del Archivo
+//System.out.println(bucket + File.separator +multipartFile.getOriginalFilename());
--- src/main/java/com/example/demo/components/FileManagment.java
+++ src/main/java/com/example/demo/components/FileManagment.java
@@ -1,11 +1,30 @@
 package com.example.demo.components;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 
 /**
+ * Componente para subir y descargar archivos del servidor
  * @author Javier Valles
  * @since  oct-11-2021
  */
+
 @Component
 public class FileManagment {
+    /**
+     * Metodo para subir archivos
+     * @param multipartFile
+     * @param name
+     * @return
+     */
+
+    public String upload(MultipartFile multipartFile, String name) throws IOException {
+        File newFile = new File(name);
+        Files.copy(multipartFile.getInputStream(), newFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+        return name;
+    }
 }
